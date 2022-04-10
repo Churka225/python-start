4239bf9de6773d7d0b1b9cea78a801292300dca1
Churka225 <100510119+Churka225@users.noreply.github.com>
2022-04-10 16:42:18 +0700

    Update drawing machine.py

--- drawing machine.py
+++ drawing machine.py
@@ -10,15 +10,18 @@ def draw():
     if mousePressed == True:
         if mouseX >= 385 and mouseX <= 535 and mouseY >= 300 and mouseY <= 400:
             background(255)
-    fill(82,1,100)
+    fill(255,3,3)
     rect(10,10,160,100)
     if mousePressed == True: 
         if mouseX >= 10 and mouseX <= 170 and mouseY >= 10 and mouseY <= 110:
-            stroke(6,152,7)
+            stroke(255,3,3)
     fill(83,14,203)
     rect(180,10,160,100)
     if mousePressed == True: 
         if mouseX >= 180 and mouseX <= 340 and mouseY >= 10 and mouseY <= 110:
-            strokeWeight(5)
-
+            strokeWeight(3)
+    fill(6,152,22)
+    rect(350,10,160,100)
+    if mouseButton == LEFT:
+        stroke(19,155,2)
             
