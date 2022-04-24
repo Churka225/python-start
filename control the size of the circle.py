1554d8ae83c41b05633801056b206cee296df4d3
Churka225 <100510119+Churka225@users.noreply.github.com>
2022-04-24 15:13:25 +0700

    Update control the size of the circle.py

--- control the size of the circle.py
+++ control the size of the circle.py
@@ -3,11 +3,11 @@ def setup():
     size(800,600)
 def draw():
     global x
+    background(255)
     ellipse(400,300,x,x)
     if key == CODED:
         if keyCode == UP:
             x = x+1
-            if keyCode == DOWN:
-                x = 0
-            
+        if keyCode == DOWN:
+            x = x-1
             
